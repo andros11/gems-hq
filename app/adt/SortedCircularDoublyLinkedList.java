03f2335fb1e96f89e31c262e8dcba1db45d6c01c
Andros Rosa <andros2011@gmail.com>
2015-03-29 09:59:10 -0400

    Add provided adt & halfway SortedCircularDLL

---
+++ app/adt/SortedCircularDoublyLinkedList.java
@@ -0,0 +1,249 @@
+package adt;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+
+public class SortedCircularDoublyLinkedList<E extends Comparable<E>> implements SortedList<E> {
+
+	private class Node {
+		private E value;
+		private Node next;
+		private Node prev;
+
+		public E getValue() {
+			return value;
+		}
+		public void setValue(E value) {
+			this.value = value;
+		}
+		public Node getNext() {
+			return next;
+		}
+		public void setNext(Node next) {
+			this.next = next;
+		}
+		public Node getPrev() {
+			return prev;
+		}
+		public void setPrev(Node prev) {
+			this.prev = prev;
+		}
+	}
+
+	private class ListIterator implements Iterator<E> {
+
+		private Node currentNode = header;
+
+		public ListIterator() {
+			currentNode = header.getNext();
+		}
+
+		public ListIterator(int index) {
+			if (index < 0 || index >= size())
+				throw new IndexOutOfBoundsException();
+
+			int i = 0;
+			for (currentNode = header.getNext(); i < index; currentNode = currentNode.getNext(), ++i);
+		}
+
+		@Override
+		public boolean hasNext() {
+			return currentNode != header;
+		}
+
+		@Override
+		public E next() {
+			if (hasNext()) {
+				E result = this.currentNode.getValue();
+				this.currentNode = this.currentNode.getNext();
+				return result;
+			}
+			else {
+				throw new NoSuchElementException();
+			}
+		}
+
+		@Override
+		public void remove() {
+			throw new UnsupportedOperationException();
+		}
+	}
+
+	private class ReverseListIterator implements ReverseIterator<E> {
+
+		private Node currentNode = header;
+
+		public ReverseListIterator() {
+			currentNode = header.getPrev();
+		}
+
+		public ReverseListIterator(int index) {
+			if (index < 0 || index >= size())
+				throw new IndexOutOfBoundsException();
+
+			int i = size() - 1;
+			for (currentNode = header.getPrev(); i > index; currentNode = currentNode.getPrev(), --i);
+		}
+
+		@Override
+		public boolean hasPrevious() {
+			return currentNode != header;
+		}
+
+		@Override
+		public E previous() {
+			if (hasPrevious()) {
+				E result = currentNode.getValue();
+				currentNode = currentNode.getPrev();
+				return result;
+			}
+			else {
+				throw new NoSuchElementException();
+			}
+		}
+	}
+
+	private Node header;
+	private int currentSize;
+
+	public SortedCircularDoublyLinkedList() {
+		this.header = new Node();
+		this.header.setNext(header);
+		this.header.setPrev(header);
+		this.currentSize = 0;
+	}
+
+	@Override
+	public Iterator<E> iterator() {
+		return new ListIterator();
+	}
+
+	@Override
+	public Iterator<E> iterator(int index) {
+		return new ListIterator(index);
+	}
+
+	@Override
+	public ReverseIterator<E> reverseIterator() {
+		return new ReverseListIterator();
+	}
+
+	@Override
+	public ReverseIterator<E> reverseIterator(int index) {
+		return new ReverseListIterator(index);
+	}
+
+	@Override
+	public boolean add(E obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean remove(E obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean remove(int index) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int removeAll(E obj) {
+		int counter = 0;
+		while (this.remove(obj)) {
+			counter++;
+		}
+		return counter;
+	}
+
+	@Override
+	public E first() {
+		return this.isEmpty() ? null : this.header.getNext().getValue();
+	}
+
+	@Override
+	public E last() {
+		return this.isEmpty() ? null : this.header.getPrev().getValue();
+	}
+
+	@Override
+	public E get(int index) {
+		if ((index < 0) || (index >= this.size())) {
+			throw new IndexOutOfBoundsException();
+		}
+
+		int counter = 0;
+		Node temp = null;
+		// closer going forward
+		if (index < this.size() / 2) {
+			for (temp = header.getNext(); counter < index; temp = temp.getNext(), counter++);
+			return temp.getValue();
+		}
+		// closer going backwards
+		else {
+			for (temp = header.getPrev(); counter > index; temp = temp.getPrev(), counter--);
+			return temp.getValue();
+		}
+	}
+
+	@Override
+	public void clear() {
+		while (!this.isEmpty()) {
+			this.remove(0);
+		}
+	}
+
+	@Override
+	public boolean contains(E e) {
+		return this.firstIndex(e) >= 0;
+	}
+
+	@Override
+	public int firstIndex(E e) {
+		if (e == null) {
+			throw new IllegalArgumentException("Parameter cannot be null.");
+		}
+		else {
+			int counter = 0;
+			Node temp = null;
+			for (temp = header.getNext(); temp != header; temp = temp.getNext(), counter++) {
+				if (temp.getValue().equals(e)) {
+					return counter;
+				}
+			}
+			return -1;
+		}
+	}
+
+	@Override
+	public int lastIndex(E e) {
+		if (e == null) {
+			throw new IllegalArgumentException("Parameter cannot be null.");
+		}
+		else {
+			int counter = 0;
+			Node temp = null;
+			for (temp = header.getPrev(); temp != header; temp = temp.getPrev(), counter--) {
+				if (temp.getValue().equals(e)) {
+					return counter;
+				}
+			}
+			return -1;
+		}
+	}
+
+	@Override
+	public int size() {
+		return this.currentSize;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return this.size() == 0;
+	}
+}
